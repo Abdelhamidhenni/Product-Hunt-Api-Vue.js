30d01712668a64c966d3a8b4311b2a92fa121607
Adelhamid Henni <abdelhamid.henni@etu-webschoolfactory.fr>
2017-03-10 12:41:44 +0100

    PostList Route Link params

--- src/components/PostsListItem/PostsListItem.vue
+++ src/components/PostsListItem/PostsListItem.vue
@@ -5,11 +5,12 @@
 <template>
 
   <ul class="posts-list-item">
-    <a :href="proppost.redirect_url">
+    <router-link :to="{ name: 'post_component', params: { postId: proppost.id } }">
     <div><img :src="proppost.thumbnail.image_url" alt=""></div>
     <h3>{{ proppost.name }}</h3>
     <li id="grey">{{proppost.votes_count}} upvotes</li>
-    <li>{{ proppost.tagline }}</li>  </a>
+    <li>{{ proppost.tagline }}</li>
+    </router-link>
 
   </ul>
 
