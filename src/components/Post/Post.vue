ad4e71e3b05831b5b1e1052397abae2c69453fc2
Adelhamid Henni <abdelhamid.henni@etu-webschoolfactory.fr>
2017-03-22 16:14:28 +0100

    Post Vue Update

--- src/components/Post/Post.vue
+++ src/components/Post/Post.vue
@@ -3,21 +3,62 @@
 
 <!-- HTML -->
 <template>
-  <div>
-    <span>{{this.$route.params.postId }}</span>
-  </div>
+    <div v-if="post">
+        <div class="app-header">
+        <div class="title">{{post.name}}</div>
+        <div class="tagline">{{post.tagline}}</div>
+        </div>
+        <div class="date">{{post.day}}</div>
+        <div class="date">{{post.body}}</div>
+        <div class="wrapper">
+            <ul class="posts-list">
+                <posts v-for="post in post.date" :proppost="date" :isInsideMaker="true" :key="post.id"></posts>
+            </ul>
+        </div>
 
+        <img :src="post.thumbnail.image_url">
+        <!--<span>{{ post.}}</span>-->
+        <div>Nombre de commentaires :{{ post.comments_count }}</div>
+    </div>
 </template>
 
 <!-- JS -->
 <script>
 
-  // Import du composant
-  //  import Post from '../components/App.vue'
+    import axios from 'axios'
+    import _ from 'lodash'
 
-  export default {
+    export default {
 
 
-  }
 
+        data() {
+            return {
+                post: null
+            }
+        },
+
+        created() {
+            this.getPost(this.$route.params.postId)
+        },
+        methods: {
+
+            getPost(postId) {
+                // Utilisez axios pour récupérer les posts de l'API ProductHunt
+                // Variable à modifier : this.posts
+                axios.get('/posts/' + postId)
+                    .then((response) => {
+                        console.log(response.data.post)
+                        console.log(response.data.postId)
+                        this.post = response.data.post
+
+                    })
+                    .catch(function (error) {
+                        console.log(error)
+                    })
+            }
+
+        }
+
+    }
 </script>
